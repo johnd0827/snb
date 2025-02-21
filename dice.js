d6d5aff81af52b34592e7b4c7a335d26aa18b96e
johnd0827 <johnd0827@gmail.com>
2025-02-21 12:20:12 -0500

    commit
    Commit

--- dice.js
+++ dice.js
@@ -1,3 +1,28 @@
+const d12_count = 0;
+const d6_count = 0;
+const d8_count = 0;
+
 function rollD12() {
     return Math.floor(Math.random() * 12) + 1;
 }
+
+function rollD6() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function rollD8() {
+    return Math.floor(Math.random() * 8) + 1;
+}
+function getD12Count() {
+    return d12_count;
+}
+
+function getD6Count() {
+    return d6_count;
+}  
+
+function getD8Count() {
+    return d8_count;
+}
+
+
