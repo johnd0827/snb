9a41179c4f74b7b97058425ca1df318bf5996e7c
johnd0827 <johnd0827@gmail.com>
2025-02-21 21:31:36 -0500

    roller

--- dice.js
+++ dice.js
@@ -14,27 +14,61 @@ let bonesCount = 0;
 
 
 function rollPositiveDice() {
-    const positiveDice = [];    
+    let positiveDice = [];    
     swordCount = 0;
     successCount = 0;
-    for (let i = 0; i < proficiencyDie; i++) {
-        let roll = rollProficiencyDie();
-        positiveDice.push(roll);
+    for (let i = 0; i < abilityDie; i++) {
+        let roll = rollAbilityDie();
+        positiveDice.push(["A", roll]);
         switch (roll) {
             case 1:
                 break;
-            case 2, 3:
+            case 2:
+                swordCount++;
+                break;
+            case 3:
                 swordCount++;
                 swordCount++;
                 break;
-            case 4, 5, 6:
+            case 4:
+            case 5:
+                successCount++;
+                break;
+            case 6:
                 successCount++;
                 swordCount++;
                 break;
-            case 7, 8:
+            case 7:
+            case 8:
+                successCount++;
                 successCount++;
                 break;
-            case 9, 10, 11:
+        }
+    }
+    for (let i = 0; i < proficiencyDie; i++) {
+        let roll = rollProficiencyDie();
+        positiveDice.push(["P", roll]);
+        switch (roll) {
+            case 1:
+                break;
+            case 2:
+            case 3:
+                swordCount++;
+                swordCount++;
+                break;
+            case 4:
+            case 5:
+            case 6:
+                successCount++;
+                swordCount++;
+                break;
+            case 7:
+            case 8:
+                successCount++;
+                break;
+            case 9:
+            case 10:
+            case 11:
                 successCount++;
                 successCount++;
                 break;
@@ -48,9 +82,126 @@ function rollPositiveDice() {
                 break;
         }
     }
-    return positiveDice;
+    for (let i = 0; i < boostDie; i++) {
+        let roll = rollBoostDie();
+        positiveDice.push(["B", roll]);
+        switch (roll) {
+            case 1:
+            case 2:
+                break;
+            case 3:
+                swordCount++;
+                break;
+            case 4:
+                successCount++;
+                successCount++;
+                break;
+            case 5:
+                swordCount++;
+                successCount++;
+                break;
+            case 6:
+                successCount++;
+                break;
+        }
+    }
+    return positiveDice.toString();
 }
 
+function rollNegativeDice() {
+    let negativeDice = [];
+    failureCount = 0;
+    bonesCount = 0;
+    for (let i = 0; i < difficultyDie; i++) {
+        let roll = rollDifficultyDie();
+        negativeDice.push(["D", roll]);
+        switch (roll) {
+            case 1:
+                break;
+            case 2:
+                failureCount++;
+                failureCount++;
+                break;
+            case 3:
+                failureCount++;
+                break;
+            case 4:
+                failureCount++;
+                bonesCount++;
+                break;
+            case 5:
+            case 6:
+            case 7:
+                bonesCount++;
+                break;
+            case 8:
+                bonesCount++;
+                bonesCount++;
+                break;
+        }                
+    }
+    for (let i = 0; i < challengeDie; i++) {
+        let roll = rollChallengeDie();
+        negativeDice.push(["C", roll]);
+        switch (roll) {
+            case 1:
+                break;
+            case 2:
+                failureCount++;
+                failureCount++;
+                failureCount++;
+                bonesCount++;
+                bonesCount++;
+                bonesCount++;
+                break;
+            case 3:
+            case 4:
+                failureCount++;
+                break;
+            case 5:
+            case 6:
+                failureCount++;
+                failureCount++;
+                break;
+            case 7:
+            case 8:
+                bonesCount++;
+                break;
+            case 9:
+            case 10:
+                failureCount++;
+                bonesCount++;
+                break;
+            case 11:
+            case 12:
+                bonesCount++;
+                bonesCount++;
+                break;
+        }
+    }
+    for (let i = 0; i < setbackDie; i++) {
+        let roll = rollSetbackDie();
+        negativeDice.push(["S", roll]);
+        switch (roll) {
+            case 1:
+            case 2:
+                break;
+            case 3:
+                failureCount++;
+                break;
+            case 4:
+                bonesCount++;
+                break;
+            case 5:
+                failureCount++;
+                break;
+            case 6:
+                bonesCount++;
+                break;
+        }
+    }   
+    return negativeDice.toString();
+}   
 //d12's
 function rollProficiencyDie() {
     return Math.floor(Math.random() * 12) + 1;
